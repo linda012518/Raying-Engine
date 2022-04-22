c9f3aca72b1fec0ce72dc32af4ce192b631daab0
linda012518 <895613747@qq.com>
2022-04-22 23:21:14 +0800

    Added check for OpenGL version

--- Raying/src/Platform/OpenGL/OpenGLContext.cpp
+++ Raying/src/Platform/OpenGL/OpenGLContext.cpp
@@ -21,6 +21,15 @@ namespace Raying
 		Raying_Core_Info("	Vendor: {0}", glGetString(GL_VENDOR));//返回负责当前OpenGL实现厂商的名字
 		Raying_Core_Info("	Renderer: {0}", glGetString(GL_RENDERER));//返回一个渲染器标识符，通常是个硬件平台
 		Raying_Core_Info("	Version: {0}", glGetString(GL_VERSION));//返回当前OpenGL实现的版本号
+
+#ifdef Raying_Enable_Asserts
+		int versionMajor;
+		int versionMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+		Raying_Core_Assert(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Hazel requires at least OpenGL version 4.5!");
+#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
