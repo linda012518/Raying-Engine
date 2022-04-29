12b3a7e40580e1c2a9ddd59e0d724ab0adb9cd02
linda012518 <895613747@qq.com>
2022-04-29 22:39:50 +0800

    `__FUNCSIG__` isn't available on all compilers

--- Raying/src/Raying/Debug/Instrumentor.h
+++ Raying/src/Raying/Debug/Instrumentor.h
@@ -124,10 +124,31 @@ namespace Raying {
 
 #define Raying_Profile 1
 #if Raying_Profile
+	// Resolve which function signature macro will be used. Note that this only
+	// is resolved when the (pre)compiler starts, so the syntax highlighting
+	// could mark the wrong one in your editor!
+	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+		#define Raying_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+		#define Raying_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__FUNCSIG__)
+		#define Raying_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+		#define Raying_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define Raying_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define Raying_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define Raying_FUNC_SIG __func__
+	#else
+		#define Raying_FUNC_SIG "Raying_FUNC_SIG unknown!"
+	#endif
+
 	#define Raying_Profile_BEGIN_SESSION(name, filepath) ::Raying::Instrumentor::Get().BeginSession(name, filepath)
 	#define Raying_Profile_END_SESSION() ::Raying::Instrumentor::Get().EndSession()
 	#define Raying_Profile_SCOPE(name) ::Raying::InstrumentationTimer timer##__LINE__(name);
-	#define Raying_Profile_FUNCTION() Raying_Profile_SCOPE(__FUNCSIG__)
+	#define Raying_Profile_FUNCTION() Raying_Profile_SCOPE(Raying_FUNC_SIG)
 #else
 	#define Raying_Profile_BEGIN_SESSION(name, filepath)
 	#define Raying_Profile_END_SESSION()
