526537ef6db41914ed0d0c5ff333b28016b9e67d
linda012518 <895613747@qq.com>
2022-05-17 21:01:20 +0800

    update Raying_Bind_Event_Fn

--- Raying/src/Raying/Core/Base.h
+++ Raying/src/Raying/Core/Base.h
@@ -82,7 +82,7 @@
 
 #define BIT(x) (1 << x)
 
-#define Raying_Bind_Event_Fn(fn) std::bind(&fn, this, std::placeholders::_1)
+#define Raying_Bind_Event_Fn(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace Raying
 {
