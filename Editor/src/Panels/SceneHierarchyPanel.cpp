735510f2fb22c27f99e90043ef98a184e074279f
linda012518 <895613747@qq.com>
2022-05-23 22:46:17 +0800

    Add Properties panel

--- Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -1,6 +1,8 @@
 #include "SceneHierarchyPanel.h"
 
 #include "Raying/Scene/Comonents.h"
+
+#include <glm/gtc/type_ptr.hpp>
 #include <imgui/imgui.h>
 
 namespace Raying {
@@ -23,6 +25,16 @@ namespace Raying {
 			_context->_registry.get<TagComponent>(entityID).Tag;
 			DrawEntityNode(entity);
 		});
+
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+			_selectionContext = {};
+
+		ImGui::End();
+
+
+		ImGui::Begin("Properties");
+		if (_selectionContext)
+			DrawComponents(_selectionContext);
 		ImGui::End();
 	}
 
@@ -39,7 +51,37 @@ namespace Raying {
 		if (opened)
 		{
 			ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_OpenOnArrow;
+			bool opened = ImGui::TreeNodeEx((void*)9817239, flags, tag.c_str());
+			if (opened)
+				ImGui::TreePop();
 			ImGui::TreePop();
+		}
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
+				ImGui::TreePop();
+			}
 		}
 	}
 
--- Editor/src/Panels/SceneHierarchyPanel.h
+++ Editor/src/Panels/SceneHierarchyPanel.h
@@ -18,6 +18,7 @@ namespace Raying {
 		void OnImGuiRender();
 	private:
 		void DrawEntityNode(Entity entity);
+		void DrawComponents(Entity entity);
 	private:
 		Ref<Scene> _context;
 		Entity _selectionContext;
